94f4da4f610586a1088625d69e871993f41fcc4e
Emma von Bergmann <emmvbe@gmail.com>
2022-11-29 17:11:44 +0100

    destroy and edit action

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -6,4 +6,26 @@ class MedicationsController < ApplicationController
   def show
     @medication = Medication.find(params[:id])
   end
+
+  def edit
+    @medication = Medication.find(params[:id])
+  end
+
+  def update
+    @medicatin = Medication.find(params[:id])
+    @medication.update(medication_params)
+    redirect_to medication_path(@medication)
+  end
+
+  def destroy
+    @medication = Medication.find(params[:id])
+    @medication.destroy
+    redirect_to medications_path, status: :see_other
+  end
+
+  private
+
+  def medication_params
+    params.require(:medication).permit(:name, :start_date, :end_date, :dose)
+  end
 end
