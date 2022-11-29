160ecaf03412d0065428dbe3a4af61e1b320b1a3
Emma von Bergmann <emmvbe@gmail.com>
2022-11-29 18:00:08 +0100

    edit a new form

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -23,6 +23,20 @@ class MedicationsController < ApplicationController
     redirect_to medications_path, status: :see_other
   end
 
+  def new
+    @medication = Medication.new
+  end
+
+  def create
+    @medication = Medication.new(medication_params)
+    @medication.user = current_user
+    if @medication.save
+      redirect_to medication_path(@medication)
+    else
+      render :new
+    end
+  end
+
   private
 
   def medication_params
