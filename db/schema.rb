fd4eef72eab9793b6d1aa37cac2d968dcfb5b70c
Marie von Braun <vonbraun.marie@gmail.com>
2022-11-29 16:03:16 +0100

    created seed files

--- db/schema.rb
+++ db/schema.rb
@@ -35,8 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_134259) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", default: "", null: false
-    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Medication.destroy_all && User.destroy_all
+
+ms_von_braun = User.create!(
+  email: "von_braun@gmail.com",
+  password: "secret",
+  password_confirmation: "secret"
+)
+
+aspirin = Medication.create!(
+  name: "Aspirin",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "1 Tab",
+  user: ms_von_braun
+)
+
+ibuprofen = Medication.create!(
+  name: "Ibuprofen",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "400mg",
+  user: ms_von_braun
+)
+
+multivitamin = Medication.create!(
+  name: "Multivitamin",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "2 Tabs",
+  user: ms_von_braun
+)
+
+magnesium = Medication.create!(
+  name: "Magnesium",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "1 Sachet",
+  user: ms_von_braun
+)
