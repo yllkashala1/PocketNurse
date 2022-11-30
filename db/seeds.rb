6bc0355df72c1b7a6225a31be26c18a1abfcb329
Yllka Shala <yllkaaa.sha@gmail.com>
2022-11-30 11:14:24 +0100

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,55 @@ magnesium = Medication.create!(
   dose: "1 Sachet",
   user: ms_von_braun
 )
+
+calcium = Medication.create!(
+  name: "Calcium",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "1 Sachet",
+  user: ms_von_braun
+
+vitamind = Medication.create!(
+  name: "Vitamin D",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "1 Sachet",
+  user: ms_von_braun
+
+vitaminb = Medication.create!(
+  name: "Vitamin B",
+  start_date: Date.new(2022, 11, 15),
+  end_date: Date.new(2022, 11, 18),
+  dose: "1 Sachet",
+  user: ms_von_braun
+
+antibiotika = Medication.create!(
+  name: "Antibiotika",
+  start_date: Date.new(2022, 12, 15),
+  end_date: Date.new(2022, 12, 18),
+  dose: "2 Tab",
+  user: ms_von_braun
+
+vitaminc = Medication.create!(
+  name: "Vitamin C",
+  start_date: Date.new(2022, 12, 10),
+  end_date: Date.new(2023, 01, 10),
+  dose: "1 Tab",
+  user: ms_von_braun
+)
+
+diclofenac = Medication.create!(
+  name: "Diclofenac",
+  start_date: Date.new(2022,12,25),
+  end_date: Date.new(2022, 12, 30),
+  dose: "2 Tab",
+  user: ms_von_braun
+)
+
+naproxen = Medication.create!(
+  name: "Naproxen",
+  start_date: Date.new(2022, 12, 26),
+  end_date: Date.new(2022, 12, 31),
+  dose: "2 Tab",
+  user: ms_von_braun
+)
