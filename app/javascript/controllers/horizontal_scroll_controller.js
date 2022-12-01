49fb40e5d18bd744b8df0c91fce1f7de824cc7cd
Yllka Shala <yllkaaa.sha@gmail.com>
2022-12-01 15:15:15 +0100

    calendar scrolling

---
+++ app/javascript/controllers/horizontal_scroll_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="horizontal-scroll"
+export default class extends Controller {
+  static targets = ["day"]
+
+  connect() {
+    this.element.scrollTo((this.dayTarget.clientWidth * 14) - 75, 0);
+  }
+}
