c3f874068df70a02b7f891873c9c52003a7df690
Emma von Bergmann <emmvbe@gmail.com>
2022-12-01 13:02:32 +0100

    logic for the days interval

--- app/controllers/intakes_controller.rb
+++ app/controllers/intakes_controller.rb
@@ -1,4 +1,12 @@
 class IntakesController < ApplicationController
-  def calendar
+  def index
+    # There is a date in params
+    if params[:date]
+      numbers = params[:date].split("-").map(&:to_i)
+      @intakes = current_user.intakes.where(date_intake: Date.new(numbers[0], numbers[1], numbers[2]))
+    # There isn't a date in params
+    else
+      @intakes = current_user.intakes.where(date_intake: Date.today)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "medications#index"
   resources :medications, only: [:index, :show, :edit, :update, :destroy, :create, :new]
-  # verb "url", to: "controller_name#action", as: :prefix (prefix_path)
-  get "calendar", to: "intakes#calendar"
+  resources :intakes, only: [:index]
 end
