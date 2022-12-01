202745e3410c27af6e05d1fa9340e80e73aae6a0
Marie von Braun <vonbraun.marie@gmail.com>
2022-12-01 14:49:02 +0100

    many changes

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,3 +1,5 @@
 class Medication < ApplicationRecord
+  attr_accessor :intake_times, :interval
+
   belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_121248) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_134327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,9 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_30_121248) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "intake_times"
     t.text "comment"
-    t.integer "interval"
     t.index ["user_id"], name: "index_medications_on_user_id"
   end
 
