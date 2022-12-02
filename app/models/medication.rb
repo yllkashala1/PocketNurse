c155e376ec0035dd362603c530aa44e6d18cd30a
Yllka Shala <yllkaaa.sha@gmail.com>
2022-12-02 14:55:40 +0100

    calendar

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -8,13 +8,23 @@ class Medication < ApplicationRecord
   # private
 
   def generate_intakes(interval, intake_times)
-    start_date = self.start_date
-    end_date = self.end_date # end_date user input inside klammern
-    date = start_date
+    # interval => ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    # intake_times => ["7:00am", "1:00pm", "7:00pm"]
+    start_date = self.start_date # Fri, 02 Dec 2022
+    end_date = self.end_date # end_date user input inside klammern # Mon, 05 Dec 2022
+    date = start_date # Fri, 02 Dec 2022
     while date <= end_date
       if interval.include?(Date::DAYNAMES[date.wday])
         intake_times.each do |intake_time|
-          intake_time_of_day = DateTime.new(date.year, date.month, date.day, intake_time[0].to_i)
+          # intake_time => "7:00am" "7:00pm"
+          if intake_time[-2..] == "12:00pm"
+            hour = 12
+          elsif intake_time[-2..] == "am"
+            hour = intake_time.split(":")[0].to_i
+          else
+            hour = intake_time.split(":")[0].to_i + 12
+          end
+          intake_time_of_day = DateTime.new(date.year, date.month, date.day, hour)
           Intake.create(expected_intake: intake_time_of_day, medication: self)
         end
       end
--- db/migrate/20221201133246_consolidate_intakes_datetimes.rb
+++ db/migrate/20221201133246_consolidate_intakes_datetimes.rb
@@ -1,6 +1,5 @@
 class ConsolidateIntakesDatetimes < ActiveRecord::Migration[7.0]
   def change
-    remove_column :intakes, :time_of_intake
     remove_column :intakes, :date_intake
     add_column :intakes, :expected_intake, :datetime
   end
